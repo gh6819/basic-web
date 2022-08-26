513aa5814ed93bcc24a3639a0d6130ad4126af37
yongho.na_ <gh6819@gmail.com>
2022-08-26 14:38:06 +0900

    input test

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -1,51 +1,28 @@
 import React, {useState} from 'react';
-import './App.css';
-import './style/style.css';
-
-const Header = () => {
-  let name = 'yongho.na_';
-
-  return <div>
-            <div className='f_bold'>{name}</div>
-        </div>;
-}
-
-const Body = ({count, setCount, history, fnHistory}) => {
-  return (<div>
-    <div>{count}</div>
-    <p>
-      <button onClick={() => {setCount(count+1)}}>증가</button>
-      <button onClick={fnHistory}>기록</button>
-    </p>
-    <span>기록내용</span>
-    <hr/>
-    <div id='historyDiv'>
-      {history.length > 0 ? history.map((count) => (
-        <div>{count}</div>
-      )) : '기록없음'}
-    </div>
-  </div>);
-}
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [history, setHistory] = useState([]);
+  const [text, setText] = useState("11");
+  const [edit, setEdit] = useState(false);
 
-  function fnHistory() {
-    setHistory([...history, count]);
-    setCount(0);
+  let content = <div>{text}<button onClick={() => setEdit(true)}>수정</button></div>
+
+  if(edit) {
+    content = (
+    <div className="App">
+      <input type="text"
+            value={text}
+            onChange={(e) => {
+                setText(e.target.value);
+            }}/>
+      <button onClick={() => {setEdit(false)}}>수정</button>
+    </div>);
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header/>
-        <Body count={count} setCount={setCount} history={history} fnHistory={fnHistory}/>
-      </header>
-    </div>
+    <>
+      {content}
+    </>
   );
 }
-
-
 
 export default App;

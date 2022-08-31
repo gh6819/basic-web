65d79d32a5f523bdabaae17c4d1dc8aeb71d8fd6
yongho.na_ <gh6819@gmail.com>
2022-09-01 08:37:18 +0900

    깃 이그노어 수정 및 기능 복습

--- src/main/java/com/react/exam/ExamApplication.java
+++ src/main/java/com/react/exam/ExamApplication.java
@@ -2,6 +2,11 @@ package com.react.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 @SpringBootApplication
 public class ExamApplication {
@@ -10,4 +15,9 @@ public class ExamApplication {
 		SpringApplication.run(ExamApplication.class, args);
 	}
 
+	@RequestMapping("/user/main.do")
+	public String mainpage(HttpServletRequest request, HttpServletResponse response, Map<String, Object> paramMap) {
+		System.out.println("모레가 다 떨어지고 시계가 멈춰버려도 쌓여간 시간만은 네곁에 네곁에");
+		return "test";
+	}
 }
